2ca9572d8b87067539edb623ea0189ddeddce953
Meetjoshi97 <80276074+Meetjoshi97@users.noreply.github.com>
2024-07-31 04:39:53 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: 'https://capstone-project-g08h.onrender.com'
 }));
 
 mongoose.connect(process.env.MONGO_URI, {
