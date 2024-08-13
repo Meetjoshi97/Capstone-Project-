8c838862523545ada2b3c0f2845576198f62942c
Meetjoshi97 <80276074+Meetjoshi97@users.noreply.github.com>
2024-08-13 17:16:56 -0400

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { createUser, getAllUsers, updateUser ,deleteUser} = require('../controllers/userController');
+const { createUser, getAllUsers, updateUser ,deleteUser,getProfile,updateUserProfile} = require('../controllers/userController');
 const router = express.Router();
+
+router.get('/profile', getProfile);
 
 router.post('/create', createUser);
 router.get('/all', getAllUsers);
 router.post('/update', updateUser);
 router.delete('/delete/:id', deleteUser);  
+router.post('/updateUserProfile',  updateUserProfile);
 
 module.exports = router;
